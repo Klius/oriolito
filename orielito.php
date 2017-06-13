65d562122f1563a5e99c9852b46a583aa289aae8
Victor Asensio <GUARDIANH2@GUARDIAN.local>
2017-06-13 16:56:55 +0200

    1st commit

---
+++ orielito.php
@@ -0,0 +1,31 @@
+<html>
+<head>  <link rel="stylesheet" type="text/css" href="arielito.css"></head>
+<body>
+	<h1 id="titulo">Oriel</h1>
+<?php
+	$oriel = "oriel";
+	$letras = ["a","e","i","o","u"];
+	for($i=0;$i<count($letras);$i++){
+		print('<div class="col">');
+		print('<h2>'.ucfirst($letras[$i]).'</h2>');
+		print('<ul>');
+		for($j=0;$j<count($letras);$j++){
+			for($k=0;$k<count($letras);$k++){
+				
+				print('<li onmouseover="showBig(this)">');
+				print(ucfirst($letras[$i]."r".$letras[$j].$letras[$k]."l"));
+				print('</li>');
+
+			}
+		}
+		print('</ul>');
+		print('</div>');
+	}
+?>
+<script>
+	function showBig(element){
+		document.getElementById("titulo").innerText = element.innerText;
+	}
+</script>
+</body>
+</html>
